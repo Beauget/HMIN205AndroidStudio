cf5a5f1392efc1a1aecb2876a8302b26e14104fd
Denis <denis.beauget@etu.umontpellier.fr>
2021-02-01 18:02:54 +0100

    Exo 5/6 TP1

---
+++ TP1EXO6/app/src/main/java/com/example/tp1/ThirdActivity.java
@@ -0,0 +1,19 @@
+package com.example.tp1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ThirdActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+
+    public void again(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
